351d551897544648962248ea637e47c852505798
bbcoyote <huffmancharles14@gmail.com>
2022-09-03 08:39:41 -0500

    added firebase db functions, still a lot of work to do.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,39 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyJplepqr0PJeewnTJQAqZVeT_CyTrTbM",
+  authDomain: "crwn-clothing-db-b75f9.firebaseapp.com",
+  projectId: "crwn-clothing-db-b75f9",
+  storageBucket: "crwn-clothing-db-b75f9.appspot.com",
+  messagingSenderId: "263462395962",
+  appId: "1:263462395962:web:23a3361a944f9dd5bb9503",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
