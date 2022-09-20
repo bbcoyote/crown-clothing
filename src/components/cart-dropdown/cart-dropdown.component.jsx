0d544d511217bd61144e7e56f4caa38581246471
bbcoyote <huffmancharles14@gmail.com>
2022-09-20 02:05:51 -0500

    added increment and decrement on the checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,31 @@
-import { useContext } from 'react';
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { CartContext } from '../../contexts/cart.context';
+import { CartContext } from "../../contexts/cart.context";
 
-import Button from '../button/button.componet';
-import CartItem from '../cart-item/cart-item.component';
+import Button from "../button/button.componet";
+import CartItem from "../cart-item/cart-item.component";
 
-import './cart-dropdown.styles.scss'
-
-
+import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-    const { cartItems } = useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-                {cartItems.map((item) => (
-                 <CartItem key={item.id} cartItem={item} />
-                 ))}
-            </div>
-            <Button>Go To Checkout</Button>
-        </div>
-    );
+  const goToCheckoutHandler = () => {
+    navigate("./checkout");
+  };
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>Go To Checkout</Button>
+    </div>
+  );
 };
 
 export default CartDropdown;
