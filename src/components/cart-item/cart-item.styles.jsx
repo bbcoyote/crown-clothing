36dc480eae1e9204cc000f77b183e69854d563d8
bbcoyote <huffmancharles14@gmail.com>
2022-09-28 17:56:19 -0500

    finishing up converting scss to styled components. Preparing to deploy with netlify!

---
+++ src/components/cart-item/cart-item.styles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const CartItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  margin-bottom: 15px;
+  img {
+    width: 30%;
+  }
+`;
+
+export const ItemDetails = styled.div`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  margin-bottom: 15px;
+  img {
+    width: 30%;
+  }
+`;
+
+export const Name = styled.span`
+  font-size: 16px;
+`;
