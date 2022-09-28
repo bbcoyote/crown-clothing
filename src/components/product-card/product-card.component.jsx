cabb751812f90c44310258d8adcf6c2f56459c4b
bbcoyote <huffmancharles14@gmail.com>
2022-09-28 05:11:19 -0500

    finishing up implementing styled components from scss files.

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,12 +1,9 @@
 import { useContext } from "react";
 
 import { CartContext } from "../../contexts/cart.context";
-import Button from "../button/button.componet";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.componet";
 
 import "./product-card.styles.scss";
-
-
-
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -21,7 +18,10 @@ const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted" onClick={addProductToCart}>
+      <Button
+        buttonType={BUTTON_TYPE_CLASSES.inverted}
+        onClick={addProductToCart}
+      >
         Add to Cart
       </Button>
     </div>
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 
 import FormInput from "../form-input/form-input.component";
-import Button from "../button/button.componet";
+import Button, { BUTTON_TYPE_CLASSES } from "../button/button.componet";
 
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
 
@@ -26,7 +25,6 @@ const SignInForm = () => {
 
   const signInWithGoogle = async () => {
     await signInWithGooglePopup();
-    
   };
 
   const handleSubmit = async (event) => {
@@ -80,7 +78,11 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type="button"
+            onClick={signInWithGoogle}
+          >
             Sign In With Google
           </Button>
         </div>
